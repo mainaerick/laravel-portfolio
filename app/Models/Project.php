65457face82471fbf83f380f3ea207716cca0139
mainaerick <niteric@gmail.com>
2025-10-29 12:23:31 +0300

    feat: setup project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    protected $fillable = [
+        'title', 'slug', 'description', 'thumbnail', 'github', 'live_url', 'meta', 'is_featured', 'order'
+    ];
+
+    protected $casts = [
+        'meta' => 'array',
+        'is_featured' => 'boolean',
+    ];
+
+    // relationships
+    public function tags() {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    // auto-slug
+    protected static function booted() {
+        static::creating(function ($project) {
+            if (empty($project->slug)) {
+                $project->slug = Str::slug($project->title);
+            }
+        });
+        static::updating(function ($project) {
+            if (empty($project->slug)) {
+                $project->slug = Str::slug($project->title);
+            }
+        });
+    }
 }
