fca973ec18809ec8b2a8c5715e74b88c2381e9dd
mainaerick <niteric@gmail.com>
2025-10-29 12:20:44 +0300

    feat: setup skills migration with columns

--- database/migrations/2025_10_29_091125_create_skills_table.php
+++ database/migrations/2025_10_29_091125_create_skills_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('skills', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('icon')->nullable(); // optional icon path/class
+            $table->unsignedInteger('order')->default(0);
             $table->timestamps();
         });
     }
