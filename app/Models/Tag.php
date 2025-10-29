d54133f3a1ce062ae1e2eb781d1d1c3a660714f9
mainaerick <niteric@gmail.com>
2025-10-29 12:24:13 +0300

    feat: setup tag model

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Tag extends Model
 {
-    //
+    protected $fillable = ['name','slug'];
+
+    public function projects() {
+        return $this->belongsToMany(Project::class);
+    }
+
+    protected static function booted() {
+        static::creating(function ($tag) {
+            if (empty($tag->slug)) {
+                $tag->slug = Str::slug($tag->name);
+            }
+        });
+    }
 }
